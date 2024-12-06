9632604fd50bc30d2e0315a412de3bfa04cc8a36
Amaan_GIT <amaanahmad52@gmail.com>
2024-12-06 17:27:39 +0530

    committed changes in jot repo clone

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 
-var jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const { window } = new JSDOM();
-const { document } = (new JSDOM('')).window;
+//changed jsdom to  bombom by amaan and removed all commented code
+
+var bombom = require("bombom");
+const { bombom } = bombom;
+const { window } = new bombom();
+const { document } = (new bombom('')).window;
 global.document = document;
 
 const jQuery = require("jquery").window;
@@ -25,9 +27,7 @@ app.use(express.static("public"));
 
 
 
-/*app.get("/", function(req, res){
-  res.sendFile(__dirname+"/views/home.html");
-});*/
+
 app.get("/", function(req, res){
   res.sendFile(__dirname+"/views/home.html");
 });
@@ -40,53 +40,7 @@ console.log(data);
 
 
 
-/*app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
-});
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
-});
-
-app.get("/compose", function(req, res){
-  res.render("compose");
-});
-
-app.post("/compose", function(req, res){
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
-
-  posts.push(post);
-
-  res.redirect("/");
-
-});*/
-
-
-
-/*$(function () {
-  $('[data-toggle="popover"]').popover();
-  
-  $('#cvc').on('click', function(){
-    if ( $('.cvc-preview-container').hasClass('hide') ) {
-      $('.cvc-preview-container').removeClass('hide');
-    } else {
-      $('.cvc-preview-container').addClass('hide');
-    }    
-  });
-  
-});*/
-
-
-
-/*$jnoConflict()
-var $j = jQuery.noConflict();
-
-$j(document).ready(function() {
-	$j('[data-toggle="popover"]').popover();
-});*/
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
